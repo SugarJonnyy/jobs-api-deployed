eacbc55fde86c96ef4adbf292603bf3f7e1bdd85
Saswata Das <220040079+SugarJonnyy@users.noreply.github.com>
2026-05-12 19:49:52 +0530

    add passoword comparison feature in user schema and jwt token

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const asyncWrapper = require('./middlewares/async')
 const errorHandlerMiddleware = require('./middlewares/errorhandler')
 const notFoundMiddleware = require('./middlewares/notFound')
 const connectDB = require('./db/connect')
+const authenticateUser = require('./middlewares/auth')
 
 const authRouter = require('./routes/auth')
 const jobsRouter = require('./routes/jobs')
@@ -14,7 +15,7 @@ const jobsRouter = require('./routes/jobs')
 app.use(express.json())
 
 app.use('/api/v1/auth', authRouter)
-app.use('/api/v1/jobs', jobsRouter)
+app.use('/api/v1/jobs', authenticateUser, jobsRouter)
 
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,34 @@
 const User = require('../models/User')
 const {StatusCodes} = require('http-status-codes')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const {badRequestError}= require('../errors/bad-request')
+const {unauthenticatedError} = require('../errors/unauthenticated')
 
 const register = async (req, res) =>{
-
-
-    const user = await User.create({...tempUser})
-    res.status(StatusCodes.CREATED).json({user})
+    const user = await User.create({...req.body})
+    const token = jwt.sign()
+    res.status(StatusCodes.CREATED).json({ user: {name: user.name}, token})
 }
 
-const login = (req, res)=>{
-    res.send(`loggin in the registerd account`)
+const login = async (req, res)=>{
+    const {email, password} = req.body
+    if(!email || !password){
+        throw new badRequestError('Please  provide email and password')
+    }
+    const user = await User.findOne({ email })
+
+    if(!user){
+        throw new unauthenticatedError('Invalid Credentials')
+    }
+
+    const isPasswordCorrect = await user.comparePassword(password)
+    if(!isPasswordCorrect){
+        throw new unauthenticatedError('Invalid Credentials')
+    }
+    
+    const token = user.createJWT()
+    res.status(StatusCodes.OK).json({user: {name: user.name}, token})
 }
 
 module.exports = {
