2c0d149c02947cdf88ee07a9fbb14d4edc519e92
Saswata Das <220040079+SugarJonnyy@users.noreply.github.com>
2026-05-15 20:52:29 +0530

    fix minor bugs

--- middlewares/errorhandler.js
+++ middlewares/errorhandler.js
@@ -3,12 +3,13 @@ const { StatusCodes } = require("http-status-codes");
 
 const errorHandlerMiddleware = (err, req, res, next) => {
     let customError = {
+        //set default
         statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
         msg: err.msg ||  `Something went wrong try again later`
     }
-  if (err instanceof customAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message });
-  }
+//   if (err instanceof customAPIError) {
+//     return res.status(err.statusCode).json({ msg: err.message });
+//   }
 
   if(err.code && err.code === 11000){
     customError.msg = `Duplicate value for ${Object.keys(err.keyValue)} field, please choose different value`
