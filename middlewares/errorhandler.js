0693ed794f888c5d59b62d448ec48e067f4565d9
Saswata Das <220040079+SugarJonnyy@users.noreply.github.com>
2026-05-16 13:10:38 +0530

    add validation error

--- middlewares/errorhandler.js
+++ middlewares/errorhandler.js
@@ -1,20 +1,25 @@
 const { customAPIError } = require("../errors/customErrorHandler");
-const { StatusCodes } = require("http-status-codes");
+const { StatusCodes, INTERNAL_SERVER_ERROR } = require("http-status-codes");
 
 const errorHandlerMiddleware = (err, req, res, next) => {
     let customError = {
         //set default
         statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
-        msg: err.msg ||  `Something went wrong try again later`
+        msg: err.message ||  `Something went wrong try again later`
     }
 //   if (err instanceof customAPIError) {
 //     return res.status(err.statusCode).json({ msg: err.message });
 //   }
+if(err.name === 'ValidationError'){
+  customError.msg = Object.values(err.errors).map((item)=>item.message).join(',')
+  customError.statusCode = 400
+}
 
   if(err.code && err.code === 11000){
     customError.msg = `Duplicate value for ${Object.keys(err.keyValue)} field, please choose different value`
     customError.statusCode = 400
   }
+  // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({msg: err}) 
   return res
     .status(customError.statusCode)
     .json({ msg: customError.msg });
